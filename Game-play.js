eda3db7272ed7c626e15c2960913c2731c8528d9
zordAAr <vermaanant77@gmail.com>
2020-10-14 17:22:35 +0530

    final code

--- Game-play.js
+++ Game-play.js
@@ -7,92 +7,70 @@ var team1={
 }
 
 var team2={
-    //Team2 details
     name: "Huesca",
     runs: [],
     score:0
 }
-
 var toss;
 
-
-// window.addEventListener("load", () => {
-
-
-// })
-
 window.onload = () =>{
-    selectToss(); // Deciding the strike
-    updateButtonText(); // update the text of the button according to the strike
-    updateNames(); // Updating the teams that are playing
+    selectToss();
+    updateButtonText();
+    updateNames();
     updateScore();
 }
 
-// function selectToss(){
-//     toss = Math.round(Math.random())+1;
-//     console.log(toss);
-// }
-// Function for deciding the toss
 var selectToss = () =>{
     toss = Math.round(Math.random())+1;
     console.log(toss);
 }
 
-// Function to change the button text
 var updateButtonText = () =>{
     var button = document.getElementById("strike-button");
     console.log(button);
     var result = document.getElementById("result");
     result.style.visibility ="";
-    // check if game is over
+    
     if(team1.runs.length == 6 && team2.runs.length == 6){
         button.remove();
-        // check if the match is draw Don't forget to use backticks ` ` and $ while calculating
         result.textContent = team1.score === team2.score ? `Its a draw`: `${team1.score > team2.score? team1.name:team2.name} Wins`;
     }
     else{
-        // check if the strike is over
         toss = team1.runs.length ===6 ? 2 : team2.runs.length === 6 ? 1 : toss;
     }
 
 
-    button.textContent = `${toss === 1 ? team1.name:team2.name} Batting`;
+    button.textContent = `${toss === 1 ? team1.name:team2.name} Strike`;
 };
 
-// Function to update the teams name that are playing
 var updateNames = () =>{
-    // Update Team - 1 name
     document.getElementById("team-1-name").textContent = team1.name;
-    // Update Team - 2 name
     document.getElementById("team-2-name").textContent = team2.name;
 }
 
 var updateScore = () =>{
-    // updating the score of team1 and team2
     document.getElementById("team-1-score").textContent = team1.score;
     document.getElementById("team-2-score").textContent = team2.score;
-    updateRuns(); //update the scoreboard
+    updateRuns();
 }
 
-// Strike button click
 var handleStrikeButtonClick = () =>{
  var runs = score[Math.floor(Math.random()*score.length)];
  console.log(runs);
 
- runs = runs === 5?"W": runs; // if run is 5 we mark it as a wicket
+ runs = runs === 5?"W": runs;
  console.log(runs);
 
- // check which team is on strike
  if (toss ===1)
    {
     team1.runs.push(runs);
-   team1.score= calculateScore(team1.runs); // Update the team score
+   team1.score= calculateScore(team1.runs);
     
    } 
    else{
     team2.runs.push(runs);
-    team2.score = calculateScore(team2.runs); // Update the team score
-    
+    team2.score = calculateScore(team2.runs);
+
    }
 
    updateButtonText();
@@ -117,7 +95,7 @@ return num =='W'? 0: num;
 var updateRuns = () =>{
     var teamOneRunsElement = document.getElementById("team-1-round-runs").children;
     var teamTwoRunsElement = document.getElementById("team-2-round-runs").children;
-    // update runs on score board
+
     team1.runs.forEach((run,index)=>{
         teamOneRunsElement[index].textContent = run;
     });
--- app.js
+++ app.js
@@ -1,55 +1,4 @@
-// let score = [0,1,2,3,4,5,6];
-// var turn ; 
-// var team1 = {
-// 	//Team 1 Details
-// 	name: "CSK",
-// 	runs: [],
-// 	score: 0
-// };
-// var team2 = {
-// 	//Team 2 Details
-// 	name: "Mumbai Indians",
-// 	runs: [],
-// 	score: 0
-// };
 
-// window.onload = () => {
-//     selectTurn(); // Decide who is gonna bat first
-//     updateButtonText(); // Update the text of the button based on the strike
-//     updateScore(); // update the initial score board
-//     updateNames(); // update the team names at the start
-// }
-
-// // Function to assign a random strike to a team at start of the match
-// let selectTurn = () => {
-//     turn = Math.round(Math.random())+1;
-//     // console.log(turn); 
-// }
-
-// let updateButtonText = () => {
-//     var button = document.getElementById("strike-button");
-//     console.log(button);
-//     var result = document.getElementById("result");
-//     console.log(result);
-//     result.style.visibility = "";
-//     //Update who is batting first
-//     button.textContent = `${turn ===1 ? team1.name: team2.name} Batting`;
-
-
-// }
-
-// let updateScore = () =>{
-//     //Update the total score of team 1
-// 	document.getElementById("team-1-score").textContent = team1.score;
-// 	//Update the total score of team 2
-// 	document.getElementById("team-2-score").textContent = team2.score;
-// }
-
-// // Function to update names of the teams
-// var updateNames = () => {
-// 	document.getElementById("team-1-name").textContent = team1.name; // Update name of team 1
-// 	document.getElementById("team-2-name").textContent = team2.name; // Update name of team 2
-// };
 
 var officers  =  [
     {  id:20, name: 'Venkatesh', years:14},
@@ -57,20 +6,12 @@ var officers  =  [
     {  id:22, name: 'Prasanth',years:7},
     {  id:23, name: 'Veeranaveen',years:6},
     ]
-    //[20,21,22,23]
-    //foreach
-    // var ids=[];
-    // officers.forEach(function (officer){
-    //     ids.push(officer.id);
-    // });
-    // console.log(ids);
     
     var ids=officers.map(function (officer){
         return officer.id;
     })
     console.log(ids);
     
-    // In ES6 notations
     var ids=officers.map((officer)=>{
         return officer.id;
     })
